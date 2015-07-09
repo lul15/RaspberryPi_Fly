25c6334d26e73a726873f1957d3d535fa93c9912
radhikadayal <radhikadayal_2015@depauw.edu>
2015-07-09 11:29:53 -0500

    Changes made

---
+++ RaspberryPi_Fly_Bootstrap/js/login.js
@@ -0,0 +1,16 @@
+<script>
+   function validate() {
+        var username = document.getElementById("usernameInput").value;
+        var password = document.getElementById("passwordInput").value;
+        if (username == null || username == "") {
+            alert("Please enter the username.");
+            return false;
+        }
+        if (password == null || password == "") {
+            alert("Please enter the password.");
+            return false;
+        }
+        alert('Login successful');
+
+    } 
+</script>
